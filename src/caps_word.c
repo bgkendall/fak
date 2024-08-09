076394f42f38b487fb4260a6613011fe6359b835
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2024-08-08 22:47:59 -0400

    Add Caps Word status indicator

--- src/caps_word.c
+++ src/caps_word.c
@@ -4,19 +4,48 @@
 
 #include "time.h"
 #include "split_central.h"
+#include "rgbled.h"
 
 __bit caps_word_state = 0;
 
+void set_caps_word_state(__bit new_state)
+{
+    if (new_state != caps_word_state)
+    {
+        caps_word_state = new_state;
+
+        if (caps_word_state)
+        {
+            neopixel_setPixelColor(0, 0xFFFFFF);
+            // neopixel_setPixelColor(1, 0xFF2222);
+            // neopixel_setPixelColor(2, 0xFF00FF);
+            // neopixel_setPixelColor(3, 0xFF2222);
+            // neopixel_setPixelColor(4, 0xFF00FF);
+            // neopixel_setPixelColor(5, 0xFF2222);
+        }
+        else
+        {
+            neopixel_setPixelColor(0, 0x000000);
+            // neopixel_setPixelColor(1, 0x000000);
+            // neopixel_setPixelColor(2, 0x000000);
+            // neopixel_setPixelColor(3, 0x000000);
+            // neopixel_setPixelColor(4, 0x000000);
+            // neopixel_setPixelColor(5, 0x000000);
+        }
+        neopixel_show();
+    }
+}
+
 void caps_word_on() {
-    caps_word_state = 1;
+    set_caps_word_state(1);
 }
 
 void caps_word_off() {
-    caps_word_state = 0;
+    set_caps_word_state(0);
 }
 
 void caps_word_toggle() {
-    caps_word_state = !caps_word_state;
+    set_caps_word_state(!caps_word_state);
 }
 
 __bit caps_word_active() {
@@ -30,7 +59,7 @@ __bit caps_word_handle_key(uint8_t code, uint8_t shift_pressed) {
 
   // check hasn't timed out
   if ((get_timer() - get_last_tap_timestamp()) > 5000) {
-    caps_word_state = 0;
+    caps_word_off();
     return 0;
   }
 
@@ -45,7 +74,7 @@ __bit caps_word_handle_key(uint8_t code, uint8_t shift_pressed) {
   } else if (code == 0x4C) { // delete
   } else {
     // otherwise: not an accepted key; disable caps word
-    caps_word_state = 0;
+    caps_word_off();
   }
 
   return 0;
